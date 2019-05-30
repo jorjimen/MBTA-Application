3664fdefd138d45f5360d2d6ac55cf5f4d678755
jorjimen <jorjimen@bu.edu>
2019-05-30 19:55:02 -0400

    Added handlind to show all the stops, both Northbound and Southbound.

---
+++ MBTA Application/Cells/Stops Cells/stopCell.swift
@@ -0,0 +1,24 @@
+//
+//  stopCell.swift
+//  MBTA Application
+//
+//  Created by Jorge Andres on 5/29/19.
+//  Copyright © 2019 Jorge Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class stopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
